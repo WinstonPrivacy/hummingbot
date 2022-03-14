78a54d2d17b9b0cc6a87916d196723ea3cb25198
WinstonPrivacy <rich@winstonprivacy.com>
2022-03-14 12:36:36 -0500

    added debug

--- hummingbot/strategy/avellaneda_market_making/avellaneda_market_making.pyx
+++ hummingbot/strategy/avellaneda_market_making/avellaneda_market_making.pyx
@@ -749,7 +749,7 @@ cdef class AvellanedaMarketMakingStrategy(StrategyBase):
             self._optimal_spread = self._gamma * mid_price_variance * time_left_fraction
             self._optimal_spread += 2 * Decimal(1 + self._gamma / self._kappa).ln() / self._gamma
 
-            self.logger().info(f"q-target: {q_target  q:  {q:.2f}  inventory adj: {-(q * self._gamma * mid_price_variance * time_left_fraction):.2f}  order book adj: {2 * Decimal(1 + self._gamma / self._kappa).ln() / self._gamma:.2f}")self.logger().info(f"q-target: {q_target  q:  {q:.2f}  inventory adj: {-(q * self._gamma * mid_price_variance * time_left_fraction):.2f}  order book adj: {2 * Decimal(1 + self._gamma / self._kappa).ln() / self._gamma:.2f}")
+            self.logger().info(f"q-target: {q_target:.2f}   q: {q:.2f}  inventory adj: {-(q * self._gamma * mid_price_variance * time_left_fraction):.2f}  order book adj: {2 * Decimal(1 + self._gamma / self._kappa).ln() / self._gamma:.2f}")self.logger().info(f"q-target: {q_target  q:  {q:.2f}  inventory adj: {-(q * self._gamma * mid_price_variance * time_left_fraction):.2f}  order book adj: {2 * Decimal(1 + self._gamma / self._kappa).ln() / self._gamma:.2f}")
 
             min_spread = price / 100 * Decimal(str(self._min_spread))
 
